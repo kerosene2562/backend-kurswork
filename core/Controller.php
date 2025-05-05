9b2925e3292e70a3aa385d139eef248334c462e8
admin <admin@MacBook-Pro-admin.local>
2025-05-05 14:02:27 +0300

    minor

---
+++ core/Controller.php
@@ -0,0 +1,13 @@
+<?php
+    namespace core;
+
+    class Controller
+    {
+        protected $template; 
+        public function __construct()
+        {
+            $path = 
+            $this->template = new Template($path);
+        }
+    }
+?>
---
+++ core/Core.php
@@ -0,0 +1,9 @@
+<?php
+    namespace core;
+
+    class Core
+    {
+        public $moduleName;
+        public $actionName;
+    }
+?>
