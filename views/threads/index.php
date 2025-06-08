7de252517d1a6eab93f6c905dd0fcf8dc3198681
admin <admin@MacBook-Pro-admin.local>
2025-06-08 15:00:59 +0300

    edited select

--- views/threads/index.php
+++ views/threads/index.php
@@ -13,7 +13,7 @@
 <body>
     <div class="threads_block">
         <?php foreach($threads as $thread) : ?>
-            <a href="/lost_island/discussion/index?thread_id=<?=$thread["id"]?>">
+            <a class="thread_card_ref" href="/lost_island/discussion/index?thread_id=<?=$thread["id"]?>">
                 <div class="thread_card">
                     <?php $media = json_decode($thread['imgs_refs'])[0]?>
                     <?php if(explode(".", $media)[1] == "mp4") : ?>
@@ -25,14 +25,17 @@
                         <img src="/lost_island/pics/<?=$media?>" class="card_img" alt="Головне зображення">
                     <?php endif; ?>
                     
+                    <div class="post_data">
+                        <p class="text"><?=$thread["created_at"]?></p>
+                    </div>
+                    <div>
+                        <p class="text"></p>
+                    </div>
                     <div class="card_text">
-                        <p><?=$thread["title"]?></p>
+                        <p class="text"><?=$thread["title"]?></p>
                     </div>
                     <div class="card_desc">
-                        <p><?=$thread["description"]?></p>
-                    </div>
-                    <div class="post_data">
-                        <p><?=$thread["created_at"]?></p>
+                        <p class="text"><?=$thread["description"]?></p>
                     </div>
                 </div>
             </a>
